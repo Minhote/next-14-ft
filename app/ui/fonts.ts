1d5d1dfe97b064ecab54214a278765f3edb8d842
Erick Icaza <erick_icaza_94@hotmail.com>
2023-11-23 16:19:45 -0500

    First Commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Montserrat, Lusitana } from 'next/font/google';
+
+export const MONSERRAT = Montserrat({ subsets: ['latin'] });
+
+export const LUSITANA = Lusitana({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
